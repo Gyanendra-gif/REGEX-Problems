36a09752d8552af70d65acd21e9ec87ad6eafb66
Gyanendra Pratap Singh Sengar <[gyanendra1252@gmail.com]>
2021-11-29 07:17:43 +0530

    [Gyanendra] Add: A Program to Check Validation of First Name

---
+++ Regex Programs/Regex Programs/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Regex_Programs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool flag = true;
+            while (flag)
+            {
+                Console.WriteLine("Enter your Choice Number to Execute the Regex Program Press- 1-Validate Name, 2-Exit");
+                int choice = Convert.ToInt32(Console.ReadLine());
+                switch (choice)
+                {
+                    case 1:
+                        Console.WriteLine("User Registration Validating using Regular Expression!");
+                        RegexSample regexSample = new RegexSample();
+                        Console.WriteLine("Enter the First Name to Check Validation:");
+                        string input = Console.ReadLine();
+                        regexSample.ValidatingFirstName(input);
+                        regexSample.Validating();
+                        break;
+                }
+            }
+        }
+    }
+}
---
+++ Regex Programs/Regex Programs/RegexSample.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace Regex_Programs
+{
+    class RegexSample
+    {
+        string pattern = "^[A-Za-z]{2,}$";
+        public void ValidatingFirstName(string input) 
+        {
+            Regex regex = new Regex(pattern);
+           
+            bool res = regex.IsMatch(input);
+            if (res)
+            {
+                Console.WriteLine("Valid Name");
+            }
+            else
+            {
+                Console.WriteLine("Invalid Name");
+            }
+        }
+        public void Validating()
+        {
+            Regex regex = new Regex(pattern);
+            Console.WriteLine("Validating the First Name");
+        }
+    }
+}
